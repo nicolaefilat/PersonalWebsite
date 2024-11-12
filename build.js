37d85d0421994d2d3802c5230de94dcbb6efda90
Nicolae Filat <nicolae.filat@gmail.com>
2024-11-12 19:48:33 +0100

    Initial commit

---
+++ build.js
@@ -0,0 +1,27 @@
+import fs from "fs";
+import yaml from "js-yaml"
+import Handlebars from "handlebars";
+
+function build() {
+    try {
+        // Load YAML data
+        const projectsData = yaml.load(
+            fs.readFileSync("projects.yaml", "utf8"),
+        );
+
+        // Load HTML template
+        const templateContent = fs.readFileSync("template.html", "utf8");
+        const template = Handlebars.compile(templateContent);
+
+        // Generate HTML by injecting data into the template
+        const outputHtml = template(projectsData);
+
+        // Write the output to a new HTML file
+        fs.writeFileSync("index.html", outputHtml);
+        console.log("index.html has been generated successfully!");
+    } catch (error) {
+        console.error("Error generating HTML:", error);
+    }
+}
+
+build();
