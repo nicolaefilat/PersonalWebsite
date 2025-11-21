2293a7230e9e4a8c97ff3b3a81b859563741f8f9
Nicolae Filat <nicolae.filat@gmail.com>
2025-11-21 14:49:05 +0100

    fix: make BrowserRouter use vite base url

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from './context/ThemeContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <App />
       </ThemeProvider>
